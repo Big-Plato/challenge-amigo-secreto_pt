6b37d1865880e95c7cb8ebc729226a897b250c77
Luis Octávio da Silva Carvalho <luisoctavius.sc@gmail.com>
2025-01-18 17:52:13 +0000

    Added function to sort friends

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+
+function adicionarAmigo () {
+    let amigo = document.getElementById('amigo').value;
+    console.log(`amigo: ${amigo}`)
+    if (amigo ===  '') {
+        alert('Informe o nome do amigo');
+    } else {
+        amigos.push(amigo);
+    }
+    amigo.value = '';
+    adicionarAmigoLista();
+}
+
+function adicionarAmigoLista () {
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = '';
+    console.log(listaAmigos);
+    
+    for (let i = 0; i < amigos.length; i++) {
+        let amigo = document.createElement('li');
+        amigo.innerHTML = amigos[i];
+        console.log(amigo);
+        listaAmigos.append(amigo);
+    }
+}
+
+function sortearAmigo () {
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let 
+}
