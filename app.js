e00f0d171a01271ca18a4754f39ca74e058b66bc
Luis Octávio da Silva Carvalho <luisoctavius.sc@gmail.com>
2025-02-02 15:20:14 +0000

    funcionalidade do botão que reinicia o sorteio

--- app.js
+++ app.js
@@ -1,45 +1,65 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = [];
+let amigoInput = document.querySelector('.button-add');
+let listaAmigos = document.getElementById('listaAmigos');
+let amigo = document.querySelector('#amigo');
+let resultado = document.querySelector('#resultado');
 
+
+amigo.addEventListener('keydown', (event) => {
+	if (event.key === 'Enter') {
+		adicionarAmigo();
+		amigo.value = '';
+	} 
+	event.stopPropagation();
+  });
 
 function adicionarAmigo () {
-    let amigo = document.getElementById('amigo').value;
-    console.log(`amigo: ${amigo}`)
-    if (amigo ===  '') {
-        alert('Informe o nome do amigo');
-    } else {
-        amigos.push(amigo);
-    }
-    amigo.value = '';
-    adicionarAmigoLista();
+	let valorAmigo = amigo.value;
+	if (valorAmigo === '') {
+		alert('Informe o nome do Amigo a ser adicionado!')
+	} else {
+		// Verifica se o valorAmigo que se quer adicionar já está na lista, se sim, lança um erro, se não adiciona o valorAmigo à lista
+		if (amigos.includes(valorAmigo)) {
+      alert(`${valorAmigo} já está na lista!`);
+			throw new Error('Amigo já está na lista!');
+		} else {
+			amigos.push(valorAmigo);
+		}
+	}
+	amigo.value = '';
+	adicionarAmigoLista();
 }
 
 function adicionarAmigoLista () {
-    let listaAmigos = document.getElementById('listaAmigos');
     listaAmigos.innerHTML = '';
     console.log(listaAmigos);
-    
+
     for (let i = 0; i < amigos.length; i++) {
-        let amigo = document.createElement('li');
-        amigo.innerHTML = amigos[i];
-        console.log(amigo);
-        listaAmigos.append(amigo);
+	let amigoListado = document.createElement('li');
+	amigoListado.innerHTML = `${amigos[i]}`;
+	console.log(amigoListado);
+	listaAmigos.append(amigoListado);
     }
 }
 
 function sortearAmigo () {
-    let resultado = document.querySelector('#resultado');
-    resultado.innerHTML = '';
-    if (amigos == '') {
-        console.log('O array está vazio!');    
-    } else {
-        let indiceAleatorio = Math.floor(Math.random() * amigos.length);
-        console.log(amigos[indiceAleatorio]);
-        let resultadoAmigo = document.createElement('li');
-        resultadoAmigo.innerHTML = amigos[indiceAleatorio];
-        resultado.appendChild(resultadoAmigo);
-    }
+	resultado.innerHTML = '';
+	if (amigos == '') {
+		alert('A lista está vazia!');
+	} else {
+		let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+		console.log(amigos[indiceAleatorio]);
+		let resultadoAmigo = document.createElement('li');
+		resultadoAmigo.innerHTML = amigos[indiceAleatorio];
+		resultado.appendChild(resultadoAmigo);
+		}
+// Trunca a lista para que se adicionem novos valores
+amigos.length = 0;
+// Apaga a lista dos amigos
+listaAmigos.innerHTML ='';		
+}
 
-    
+function reiniciarSorteio() {
+  resultado.innerHTML = "";
 }
